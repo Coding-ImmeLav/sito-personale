da541037c0502ddb8263f6aa11485412967d3a92
Federico Pozzi <117451142+federicoImmeLav@users.noreply.github.com>
2025-11-20 10:58:37 +0100

    m

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-ore = 999;
+let ore = 999;
 
 function assenze() {
     n = parseInt(document.getElementById("progresso").value);
     console.log(n);
-    assenze = n * 6;
-    ore_assenti = ore - assenze;
-    percentuale = (100 * ore_assenti)/ore;
+    let assenze = n * 6;
+    let ore_assenti = ore - assenze;
+    let percentuale = (100 * ore_assenti)/ore;
     console.log(percentuale);
     document.getElementById("barra").style.width = percentuale + "%";
 }
